dc1441113d82035980d03a3ae11c4c70606a4919
Andrew Fruth <anfruth@gmail.com>
2018-02-01 19:48:21 -0600

    Initial Commit

---
+++ Marcus Jr. Meditations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marcus Jr. Meditations
+//
+//  Created by Andrew Fruth on 2/1/18.
+//  Copyright © 2018 Andrew Fruth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

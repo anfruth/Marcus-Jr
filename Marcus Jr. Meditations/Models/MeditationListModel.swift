8306871bf2c4d0a9e0dfd52488c8b85070b354d6
Andrew Fruth <anfruth@gmail.com>
2018-03-18 16:42:38 -0500

    Add meditation list model to store completed exercises

---
+++ Marcus Jr. Meditations/Models/MeditationListModel.swift
@@ -0,0 +1,42 @@
+//
+//  MeditationListModel.swift
+//  Marcus Jr. Meditations
+//
+//  Created by Andrew Fruth on 3/18/18.
+//  Copyright © 2018 Andrew Fruth. All rights reserved.
+//
+
+import Foundation
+
+protocol CompleteExerciseSettable {} // only classes or structs that conform to this protocol can change the completedExercises dict.
+
+class MeditationList {
+    
+    fileprivate(set) static var completedExercises: [String: Bool] = [:] // faster lookup time with dictionary than with array, even though all values will be true
+    
+    static func retrieveMeditationListFromDisk() {
+        if let completedExercisesFromDisk = UserDefaults.standard.dictionary(forKey: "completedExercises") as? [String: Bool] {
+            completedExercises = completedExercisesFromDisk
+        } else {
+            completedExercises = [:]
+        }
+    }
+    
+}
+
+extension CompleteExerciseSettable {
+    
+    static func completeExercise(exercise: String) {
+        MeditationList.completedExercises[exercise] = true
+    }
+    
+    static func resetExercise(exercise: String) {
+        MeditationList.completedExercises[exercise] = nil
+    }
+    
+    static func saveMeditationListToDisk() {
+        UserDefaults.standard.set(MeditationList.completedExercises, forKey: "completedExercises")
+        
+    }
+    
+}

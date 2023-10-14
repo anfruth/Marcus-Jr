6606e05edaac42628908f658dbad4749a58b1191
Andrew Fruth <anfruth@gmail.com>
2023-10-14 17:19:35 -0600

    Refactor meditation view

--- Marcus Jr. Meditations/SwiftUIv2/Views/MeditationView.swift
+++ Marcus Jr. Meditations/SwiftUIv2/Views/MeditationView.swift
@@ -19,65 +19,78 @@ struct MeditationView: View, MeditationNavigating {
     @Binding var isShowingMeditationList: Bool
 
     var body: some View {
-        VStack {
-            ScrollView {
-                Text("Quotation:")
-                    .bold()
+        ScrollView {
+            VStack(alignment: .leading, spacing: 5) {
+                HStack {
+                    Spacer()
+                    Text("Quotation:")
+                        .bold()
+                    Spacer()
+                }
                 Text(viewModel.quotation)
                     .padding()
                 
                 if viewModel.commentaryAvailable {
-                    Text("Commentary:")
-                        .bold()
+                    HStack {
+                        Spacer()
+                        Text("Commentary:")
+                            .bold()
+                        Spacer()
+                    }
                     Text(viewModel.commentary)
                         .padding()
                 }
                 
                 if viewModel.actionAvailable {
-                    Text("Action:")
-                        .bold()
+                    HStack {
+                        Spacer()
+                        Text("Action:")
+                            .bold()
+                        Spacer()
+                    }
                     Text(viewModel.action)
                         .padding()
+                    
                 }
             }
-            .padding([.top, .bottom])
-            .navigationTitle(viewModel.enchiridionChapter)
-            .navigationBarItems(leading: Button(action: { dismiss() }, label: {
-                Image(systemName: "chevron.left")
-                    .foregroundColor(Color(uiColor: .label))
-            }))
-            .navigationBarBackButtonHidden()
-            
-            NavigationLink(destination: viewModel.meditationDatesView(isShowingMeditationList: $isShowingMeditationList), isActive: $viewModel.dateSettingAllowed) { EmptyView() }
-                .isDetailLink(false)
-            
-            MarcusCommonButton(title: "Set Meditation Times") {
-                if !viewModel.dateSettingAllowed {
-                    Task { @MainActor in
-                        await viewModel.getNotificationPermissionIfNeeded()
-                    }
+        }
+        .padding([.top, .bottom])
+        .navigationTitle(viewModel.enchiridionChapter)
+        .navigationBarItems(leading: Button(action: { dismiss() }, label: {
+            Image(systemName: "chevron.left")
+                .foregroundColor(Color(uiColor: .label))
+        }))
+        .navigationBarBackButtonHidden()
+        
+        NavigationLink(destination: viewModel.meditationDatesView(isShowingMeditationList: $isShowingMeditationList), isActive: $viewModel.dateSettingAllowed) { EmptyView() }
+            .isDetailLink(false)
+        
+        MarcusCommonButton(title: "Set Meditation Times") {
+            if !viewModel.dateSettingAllowed {
+                Task { @MainActor in
+                    await viewModel.getNotificationPermissionIfNeeded()
                 }
             }
-            .alert(viewModel.alertInfo?.title ?? "", isPresented: $viewModel.showAlert, actions: {
-                Button(viewModel.alertInfo?.acceptActionOption ?? "") {
-                    if viewModel.alertInfo?.acceptAction?() == true {
-                        openSettingsUrl()
-                    }
+        }
+        .alert(viewModel.alertInfo?.title ?? "", isPresented: $viewModel.showAlert, actions: {
+            Button(viewModel.alertInfo?.acceptActionOption ?? "") {
+                if viewModel.alertInfo?.acceptAction?() == true {
+                    openSettingsUrl()
                 }
-                Button(viewModel.alertInfo?.declineActionOption ?? "") {
-                    viewModel.alertInfo?.declineAction?()
-                }
-            }, message: {
-                Text(viewModel.alertInfo?.message ?? "")
-            })
-            .padding([.bottom])
-            .onAppear {
-                handleRoutingOnAppear()
-                viewModel.markAsCompleteIfComplete(timeVisited: Date.now)
             }
-            .onChange(of: routingState.isActive) { isActive in
-                handleRoutingOnChange()
+            Button(viewModel.alertInfo?.declineActionOption ?? "") {
+                viewModel.alertInfo?.declineAction?()
             }
+        }, message: {
+            Text(viewModel.alertInfo?.message ?? "")
+        })
+        .padding([.bottom])
+        .onAppear {
+            handleRoutingOnAppear()
+            viewModel.markAsCompleteIfComplete(timeVisited: Date.now)
+        }
+        .onChange(of: routingState.isActive) { isActive in
+            handleRoutingOnChange()
         }
     }
     
@@ -109,9 +122,22 @@ struct MeditationView_Previews: PreviewProvider {
         return meditation
     }
     
+    static var secondMeditation: Meditation {
+        let meditation = Meditation(context: DataController.sharedInstance.container.viewContext)
+        meditation.localizedId = "02Desire_things"
+        meditation.visitedAfterFinalTime = false
+        return meditation
+    }
+    
     static var previews: some View {
         NavigationView {
             MeditationView(viewModel: MeditationViewModel(meditation: meditation, emotionDescription: EmotionDescription(context: DataController.sharedInstance.container.viewContext)), isShowingMeditationList: .constant(true))
+                .environmentObject(NotificationsReceiver.sharedInstance.routingState)
+        }
+        .navigationViewStyle(.stack)
+        
+        NavigationView {
+            MeditationView(viewModel: MeditationViewModel(meditation: secondMeditation, emotionDescription: EmotionDescription(context: DataController.sharedInstance.container.viewContext)), isShowingMeditationList: .constant(true))
                 .environmentObject(NotificationsReceiver.sharedInstance.routingState)
         }
         .navigationViewStyle(.stack)
